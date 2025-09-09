9088d129331ed1e7a7eefa32e3c5a271606a35dc
brianmuigai2-stack <brian.muigai2@student.moringaschool.com>
2025-09-09 11:33:43 +0300

    finished speed detector challenge

--- script.js
+++ script.js
@@ -23,5 +23,49 @@ document.getElementById("gradeForm").addEventListener("submit", function(event) 
     const score = parseInt(document.getElementById("bmm").value);
     const grade = marks(score);
 
-    document.getElementById("result").innerText = `Grade: ${grade}`;
+    document.getElementById("gradeResult").innerText = `Grade: ${grade}`;
+});
+
+function speed(velocity) {
+  if (velocity <= 70) {
+    return "Ok";
+  }else if (velocity >70 && velocity < 75) {
+    return "points 0"
+  }
+  else if (velocity >= 75 && velocity < 80){
+    return "Point: 1"; 
+  }else if (velocity >= 80 && velocity < 85) {
+    return "points: 2"
+  }else if (velocity >= 85 && velocity < 90) {
+    return "points: 3"
+  }else if (velocity >= 90 && velocity < 95){
+    return "Point: 4"; 
+  }else if (velocity >= 95 && velocity < 100) {
+    return "points: 5"
+  }else if (velocity >= 100 && velocity < 105) {
+    return "points: 6"
+  }else if (velocity >= 105 && velocity < 110){
+    return "Point: 7"; 
+  }else if (velocity >= 110 && velocity < 115) {
+    return "points: 8"
+  }else if (velocity >= 115 && velocity < 120) {
+    return "points: 9"
+  }else if (velocity >= 120 && velocity < 125){
+    return "Point: 10"; 
+  }else if (velocity >= 125 && velocity < 130) {
+    return "points: 11"
+  }else if (velocity >= 130 && velocity < 135) {
+    return "points: 12"
+  }else if(velocity > 135) {
+    return "License suspended"
+  }
+}
+
+document.getElementById("detector").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const velocity = parseInt(document.getElementById("mmw").value);
+    const answer = speed(velocity);
+
+    document.getElementById("speedResult").innerText = `Speed check: ${answer}`;
 });
